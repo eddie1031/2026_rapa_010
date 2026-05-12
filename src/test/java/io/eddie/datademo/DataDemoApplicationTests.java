94d708d14b20e879e151dcb3646e1d6bb08b8eab
eddie1031 <archaco123@gmail.com>
2026-05-12 10:24:47 +0900

    init

---
+++ src/test/java/io/eddie/datademo/DataDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eddie.datademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/io/eddie/datademo/domain/Item.java
@@ -0,0 +1,47 @@
+package io.eddie.datademo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String code;
+
+    private Integer price;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Item(String name, String code, Integer price) {
+        this.name = name;
+        this.code = code;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(code, item.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code);
+    }
+
+}
